d10d675f88208d15947115af28d6a85ffa75c6bd
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-07-12 04:38:46 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,9 +1,8 @@
 def using_until
   levitation_force = 6
-  while levitation_force < 10 
-  levitation_force += 1 
-  puts "Wingardium Levoisa"
-end
-    
+  until levitation_force == 10 
+    puts "Wingardium Leviosa"
+    levitation_force += 1 
+  end
 end
 
--- lib/while.rb
+++ lib/while.rb
@@ -1,11 +1,7 @@
 def using_while
   levitation_force = 6
-	10 >.times do 
-	  puts "Wingardium Leviosa"
-	  
-	  if counter 10 >
-	    break
-	  end
+	
+
 end
 
 
